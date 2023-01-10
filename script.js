9940aa2da06df826362776898d49f2262a700df5
Andrei Grigoruta <androk20@gmail.com>
2023-01-10 12:21:32 +0200

    Add playRound function

--- script.js
+++ script.js
@@ -14,4 +14,31 @@ function getComputerChoice() {
   }
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+  const player = playerSelection.toLowerCase();
+  const playerS = player.charAt(0).toUpperCase() + player.slice(1);
+
+  switch (playerS) {
+    case "Rock":
+      if (computerSelection === "Rock") {
+        return "Draw!";
+      } else if (computerSelection === "Scissors") {
+        return "You Win! Rock beats Scissors!";
+      } else return "You Lose! Paper beats Rock!";
+      break;
+    case "Scissors":
+      if (computerSelection === "Scissors") {
+        return "Draw!";
+      } else if (computerSelection === "Paper") {
+        return "You Win! Scissors beats Paper!";
+      } else return "You Lose! Rock beats Scissors!";
+      break;
+    case "Paper":
+      if (computerSelection === "Paper") {
+        return "Draw!";
+      } else if (computerSelection === "Rock") {
+        return "You Win! Paper beats Rock!";
+      } else return "You Lose! Scissors beats Paper!";
+      break;
+  }
+}
